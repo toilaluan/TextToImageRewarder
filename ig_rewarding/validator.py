04fcdda8d9116fc6f3d09000f3e5434ffdd43cfe
thanhduycao <thanhduycao1202@gmail.com>
2023-11-01 00:40:35 +0000

    hot fix

--- ig_rewarding/validator.py
+++ ig_rewarding/validator.py
@@ -12,8 +12,8 @@ class Validator(nn.Module):
         if prompter_cfg:
             self.prompter = instantiate_from_config(prompter_cfg).to(device)
 
-    def generate_prompt(self, topics: List[str]):
-        return self.prompter.generate_prompt(topics)
+    def generate_prompt(self, topics: List[str], n_prompts: int = 100):
+        return self.prompter.generate_prompt(topics, n_prompts)
 
     def get_reward_score(self, images: List[Image.Image], prompt: str):
         return self.rewarder(images, prompt)
